b71755616570cfe04497b06298642659a41f8186
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2025-01-13 15:15:42 +0100

    make reexport for helpers

--- helpers/index.js
+++ helpers/index.js
@@ -4,8 +4,23 @@ const handleMongooseError=require('./handleMongooseError')
 const createHash=require('./createHash')
 const handleNotFoundRequest=require('./handleNotFoundRequest')
 const handleInternalErrors=require('./handleInternalErrors')
+const sendEmail=require('./sendEmail')
+const calculateTotalMinutes=require('./calculateTotalMinutes')
+const calculateTotalEarnings=require('./calculateTotalEarnings')
+const WORKING_DAY_TYPES=require('./workingDayTypes')
+const DAY_NAMES=require('./dayNames')
 
 
 module.exports={
-    HttpError,ctrlWrapper,handleMongooseError,createHash,handleNotFoundRequest,handleInternalErrors
+    HttpError,
+    ctrlWrapper,
+    handleMongooseError,
+    createHash,
+    handleNotFoundRequest,
+    handleInternalErrors,
+    sendEmail,
+    calculateTotalMinutes,
+    calculateTotalEarnings,
+    WORKING_DAY_TYPES,
+    DAY_NAMES
 }
