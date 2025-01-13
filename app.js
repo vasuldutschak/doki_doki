25a2c5c861895418a426c8387a68fece03a90945
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2025-01-13 15:30:56 +0100

    add routs to main app

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ require('dotenv').config()
 
 const authRouter=require('./routes/api/auth')
 const usersRouter=require('./routes/api/users')
+const schedulesRouter=require('./routes/api/schedule')
+const statsRouter=require('./routes/api/stats')
 
 const app=express()
 
 app.use(cors())
 app.use(express.json())
+
 app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/users",usersRouter)
+app.use("/api/v1/schedules",schedulesRouter)
+app.use("/api/v1/stats",statsRouter)
+
 app.use(handleNotFoundRequest)
 app.use(handleInternalErrors)
 
